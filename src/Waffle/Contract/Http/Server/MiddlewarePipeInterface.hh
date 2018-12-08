0f5b4ce79d354490eca1726ec9a7600eb19959f6
azjezz <azjezz@protonmail.com>
2018-12-08 13:18:50 +0100

    add doc comment to the middleware pipe interface.

--- src/Waffle/Contract/Http/Server/MiddlewarePipeInterface.hh
+++ src/Waffle/Contract/Http/Server/MiddlewarePipeInterface.hh
@@ -2,6 +2,16 @@
 
 namespace Waffle\Contract\Http\Server;
 
+/**
+ * Pipe middleware like unix pipes.
+ *
+ * This class implements a pipeline of middleware, which can be attached using
+ * the `pipe()` method, and is itself middleware.
+ *
+ * Inspired by Sencha Connect.
+ *
+ * @see https://github.com/senchalabs/connect
+ */
 interface MiddlewarePipeInterface extends MiddlewareInterface, RequestHandlerInterface
 {
     public function pipe(MiddlewareInterface $middleware): void;
