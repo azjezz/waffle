6beb61a89be7363f06d6b7c89bf091607aff26c7
azjezz <azjezz@protonmail.com>
2018-12-09 18:03:17 +0100

    define constatns for common http request methods.

---
+++ src/Waffle/Http/Message/RequestMethod.hh
@@ -0,0 +1,22 @@
+<?hh // strict
+
+namespace Waffle\Http\Message;
+
+/**
+ * Defines constants for common HTTP request methods.
+ */
+final class RequestMethod
+{
+    const METHOD_HEAD    = 'HEAD';
+    const METHOD_GET     = 'GET';
+    const METHOD_POST    = 'POST';
+    const METHOD_PUT     = 'PUT';
+    const METHOD_PATCH   = 'PATCH';
+    const METHOD_DELETE  = 'DELETE';
+    const METHOD_PURGE   = 'PURGE';
+    const METHOD_OPTIONS = 'OPTIONS';
+    const METHOD_TRACE   = 'TRACE';
+    const METHOD_CONNECT = 'CONNECT';
+
+    private function __construct() {}
+}
