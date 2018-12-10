69d52d4d530c9cff3315d0f9e8a547e5b15a2436
azjezz <azjezz@protonmail.com>
2018-12-10 22:23:19 +0100

    add more http request methods constants.

--- src/Waffle/Http/Message/RequestMethod.hh
+++ src/Waffle/Http/Message/RequestMethod.hh
@@ -7,16 +7,24 @@ namespace Waffle\Http\Message;
  */
 final class RequestMethod
 {
-    const METHOD_HEAD    = 'HEAD';
-    const METHOD_GET     = 'GET';
-    const METHOD_POST    = 'POST';
-    const METHOD_PUT     = 'PUT';
-    const METHOD_PATCH   = 'PATCH';
-    const METHOD_DELETE  = 'DELETE';
-    const METHOD_PURGE   = 'PURGE';
-    const METHOD_OPTIONS = 'OPTIONS';
-    const METHOD_TRACE   = 'TRACE';
-    const METHOD_CONNECT = 'CONNECT';
-
+    const string METHOD_HEAD        = 'HEAD';
+    const string METHOD_GET         = 'GET';
+    const string METHOD_POST        = 'POST';
+    const string METHOD_PUT         = 'PUT';
+    const string METHOD_PATCH       = 'PATCH';
+    const string METHOD_DELETE      = 'DELETE';
+    const string METHOD_PURGE       = 'PURGE';
+    const string METHOD_OPTIONS     = 'OPTIONS';
+    const string METHOD_TRACE       = 'TRACE';
+    const string METHOD_CONNECT     = 'CONNECT';
+    const string METHOD_REPORT      = 'REPORT';
+    const string METHOD_LOCK        = 'LOCK';
+    const string METHOD_UNLOCK      = 'UNLOCK';
+    const string METHOD_COPY        = 'COPY';
+    const string METHOD_MOVE        = 'MOVE';
+    const string METHOD_MERGE       = 'MERGE';
+    const string METHOD_NOTIFY      = 'NOTIFY';
+    const string METHOD_SUBSCRIBE   = 'SUBSCRIBE';
+    const string METHOD_UNSUBSCRIBE = 'UNSUBSCRIBE';
     private function __construct() {}
 }
