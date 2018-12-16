f9c55f6cba42a0c4cbc0d0a0e8a21072cfbf569e
azjezz <azjezz@protonmail.com>
2018-12-16 21:54:34 +0100

    [Event] add event interface description

--- src/Waffle/Contract/Event/EventInterface.hh
+++ src/Waffle/Contract/Event/EventInterface.hh
@@ -2,6 +2,13 @@
 
 namespace Waffle\Contract\Event;
 
+/**
+ * Marker interface indicating an event instance.
+ *
+ * Event instances may contain zero methods, or as many methods as they
+ * want. The interface MUST be implemented, however, to provide type-safety
+ * to both listeners as well as the dispatcher.
+ */
 interface EventInterface
 {
 }
