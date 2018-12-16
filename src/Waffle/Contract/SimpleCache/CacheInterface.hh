b6623268297cae214a46f26b0962a651ddba57f3
azjezz <azjezz@protonmail.com>
2018-12-17 00:16:20 +0100

    typo

--- src/Waffle/Contract/SimpleCache/CacheInterface.hh
+++ src/Waffle/Contract/SimpleCache/CacheInterface.hh
@@ -60,17 +60,17 @@ interface CacheInterface
      * @param iterable $keys    A list of keys that can obtained in a single operation.
      * @param mixed    $default Default value to return for keys that do not exist.
      *
-     * @return iterable A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
+     * @return KeyedContainer<string, mixed> A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
      */
-    public function getMultiple(Container<string> $keys,mixed $default = null): Container<mixed>;
+    public function getMultiple(Container<string> $keys,mixed $default = null): KeyedContainer<string, mixed>;
 
     /**
      * Persists a set of key => value pairs in the cache, with an optional TTL.
      *
-     * @param iterable                $values A list of key => value pairs for a multiple-set operation.
-     * @param null|DateInterval       $ttl    Optional. The TTL value of this item. If no value is sent and
-     *                                       the driver supports TTL then the library may set a default value
-     *                                       for it or let the driver take care of that.
+     * @param KeyedContainer<string, mixed> $values A list of key => value pairs for a multiple-set operation.
+     * @param null|DateInterval             $ttl    Optional. The TTL value of this item. If no value is sent and
+     *                                              the driver supports TTL then the library may set a default value
+     *                                              for it or let the driver take care of that.
      *
      * @return bool True on success and false on failure.
      */
@@ -79,7 +79,7 @@ interface CacheInterface
     /**
      * Deletes multiple cache items in a single operation.
      *
-     * @param iterable $keys A list of string-based keys to be deleted.
+     * @param Container<string> $keys A list of string-based keys to be deleted.
      *
      * @return bool True if the items were successfully removed. False if there was an error.
      */
