78f9e574129ca7dd198a4504d292b9eab6307667
azjezz <azjezz@protonmail.com>
2018-12-16 23:58:37 +0100

    typo

--- src/Waffle/Contract/SimpleCache/CacheInterface.hh
+++ src/Waffle/Contract/SimpleCache/CacheInterface.hh
@@ -74,7 +74,7 @@ interface CacheInterface
      *
      * @return bool True on success and false on failure.
      */
-    public function setMultiple(Container<mixed> $values,?DateInterval $ttl = null): bool;
+    public function setMultiple(KeyedContainer<string, mixed> $values,?DateInterval $ttl = null): bool;
 
     /**
      * Deletes multiple cache items in a single operation.
