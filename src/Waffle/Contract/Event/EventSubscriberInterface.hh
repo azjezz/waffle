b304067591a13b96ecae46cd26fed06904be5238
azjezz <azjezz@protonmail.com>
2018-12-16 21:55:09 +0100

    [Event] add event subscriber interface description

--- src/Waffle/Contract/Event/EventSubscriberInterface.hh
+++ src/Waffle/Contract/Event/EventSubscriberInterface.hh
@@ -5,7 +5,7 @@ namespace Waffle\Contract\Event;
 interface EventSubscriberInterface
 {
     /*
-     *
+     * Subscribe to the event dispatcher.
      */
     public function subscribe(EventDispatcherInterface $events): void;
 }
