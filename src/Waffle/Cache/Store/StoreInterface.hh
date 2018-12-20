be50ecf71b4c0da14255438e9fae0c1b8b01fe4f
azjezz <azjezz@protonmail.com>
2018-12-21 00:26:53 +0100

    [Cache] update store interface

--- src/Waffle/Cache/Store/StoreInterface.hh
+++ src/Waffle/Cache/Store/StoreInterface.hh
@@ -3,7 +3,17 @@
 namespace Waffle\Cache\Store;
 
 use type Waffle\Contract\Cache\CacheItemPoolInterface;
+use type Waffle\Contract\Service\ResetInterface;
 
-interface StoreInterface extends CacheItemPoolInterface
+interface StoreInterface extends ResetInterface
 {
+    public function store(string $id, mixed $value, num $ttl = 0): bool;
+
+    public function contains(string $id): bool;
+
+    public function delete(string $id): bool;
+
+    public function get(string $id): mixed;
+
+    public function prune(): bool;
 }
