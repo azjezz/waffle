3a1bab6731b5f4aac98f04f120e586bef8620508
azjezz <azjezz@protonmail.com>
2018-12-21 00:27:06 +0100

    [Cache] implement redis store

---
+++ src/Waffle/Cache/Store/RedisStore.hh
@@ -0,0 +1,66 @@
+<?hh // strict
+
+namespace Waffle\Cache\Store;
+
+use type Waffle\Cache\Serializer\SerializerInterface;
+use type Waffle\Cache\Serializer\DefaultSerializer;
+use type Waffle\Contract\Cache\CacheItemInterface;
+use type Waffle\Cache\CacheItem;
+use type Redis;
+
+class RedisStore implements StoreInterface
+{
+    public function __construct(
+        protected Redis $redis,
+        protected ?int $defaultLifetime = 0,
+        protected SerializerInterface $serializer = new DefaultSerializer()
+    ) {
+        $redis->ping();
+    }
+
+    public function get(string $key): mixed
+    {
+        if (!$this->contains($key)) {
+            return null;
+        }
+
+        return $this->serializer->unserialize(
+            (string) $this->redis->get($key)
+        );
+    }
+
+    public function delete(string $key): bool
+    {
+        $this->redis->del($key);
+        return true;
+    }
+
+    public function contains(string $key): bool
+    {
+        return (bool) $this->redis->exists($key);
+    }
+
+    public function store(string $id, mixed $value, num $ttl = 0): bool
+    {
+        $value = $this->serializer->serialize($value);
+
+        if (0 >= $ttl) {
+            return (bool) $this->redis->set($id, $value);
+        } else {
+            return (bool) $this->redis->setex($id, $ttl, $value);
+        }
+    }
+
+    public function prune(): bool
+    {
+        /*
+         * Redis deletes items automatically after the timeout has expired.
+         */
+        return true;
+    }
+
+    public function reset(): void
+    {
+        // noop
+    }
+}
