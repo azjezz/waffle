a8d131a7577e844a8f9c78b32de8d84a9084b8c0
azjezz <azjezz@protonmail.com>
2018-12-23 14:27:38 +0100

    [Config] add ini file provider

---
+++ src/Waffle/Config/Provider/IniFileProvider.hh
@@ -0,0 +1,25 @@
+<?hh // strict
+
+namespace Waffle\Config\Provider;
+
+use type Generator;
+use function parse_ini_file;
+use const INI_SCANNER_NORMAL;
+
+class IniFileProvider extends MultipleResourcesProvider
+{
+    use GlobTrait;
+
+    public function __construct(
+        protected string $pattern,
+        protected bool $processSections = true,
+        protected int $scannerMode = INI_SCANNER_NORMAL
+    ) {}
+
+    protected function loadMany(): Generator<string, KeyedContainer<arraykey, mixed>, void>
+    {
+        foreach ($this->glob($this->pattern) as $file) {
+            yield $file => parse_ini_file($file, $this->processSections, $this->scannerMode);
+        }
+    }
+}
