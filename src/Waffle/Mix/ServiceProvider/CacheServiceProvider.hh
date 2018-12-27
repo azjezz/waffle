56039d8068f079015d23f01af688ec774ec5f544
azjezz <azjezz@protonmail.com>
2018-12-27 14:24:29 +0100

    [Mix] improve cache service provider

--- src/Waffle/Mix/ServiceProvider/CacheServiceProvider.hh
+++ src/Waffle/Mix/ServiceProvider/CacheServiceProvider.hh
@@ -2,17 +2,18 @@
 
 namespace Waffle\Mix\ServiceProvider;
 
-use namespace HH\Lib\C;
 use namespace Waffle\Cache\Store;
 use namespace Waffle\Cache\Serializer;
-use type Waffle\Container\ServiceProvider\AbstractServiceProvider;
-use type Waffle\Contract\Config\ConfigurationInterface;
-use type Waffle\Mix\Environment;
 use type Waffle\Contract\Cache\CacheItemPoolInterface;
+use type Waffle\Contract\SimpleCache\CacheInterface;
+use type Waffle\Contract\Config\ConfigurationInterface;
+use type Waffle\Container\ServiceProvider\AbstractServiceProvider;
+use type Waffle\Container\Argument\RawArgument;
 use type Waffle\Cache\CacheItemPool;
 use type Waffle\Cache\Store\StoreInterface;
 use type Waffle\Cache\Serializer\SerializerInterface;
-use type Waffle\Container\Argument\RawArgument;
+use type Waffle\Mix\Environment;
+use type Waffle\SimpleCache\Cache;
 use type Redis;
 
 class CacheServiceProvider extends AbstractServiceProvider
@@ -21,67 +22,68 @@ class CacheServiceProvider extends AbstractServiceProvider
         CacheItemPoolInterface::class,
         SerializerInterface::class,
         StoreInterface::class,
+        Store\RedisStore::class,
+        Store\ArrayStore::class,
+        Store\NullStore::class,
+        Store\ApcStore::class,
     ];
 
     <<__Override>>
     public function register(): void
     {
         $config = $this->getContainer()->get('config') as ConfigurationInterface
-                |> $$->get('cache') as ?KeyedContainer<_, _>;
-        $config = $config ?? dict[];
+                |> $$->get('cache', shape()) as shape(
+                    ?'store' => string,
+                    ?'namespace' => string,
+                    ?'default_ttl' => int,
+                    ...
+                );
         $env = $this->getContainer()->get('env') as Environment;
 
         $this->share(CacheItemPoolInterface::class, CacheItemPool::class)
             ->addArgument(StoreInterface::class);
         $this->share(SerializerInterface::class, Serializer\DefaultSerializer::class);
 
-        if (C\contains_key($config, 'store')) {
-            /* HH_IGNORE_ERROR[4110] */
-            $store = $config['store'] as string;
-        } else {
-            if ($env->debug()) {
-                $store = 'null';
-            } else {
-                $store = $env->dev() ? 'array' : 'apc';
-            }
-        }
-
-        if (C\contains_key($config, 'namespace')) {
-            /* HH_IGNORE_ERROR[4110] */
-            $namespace = $config['namespace'] as string;
-        } else {
-            $namespace = '';
-        }
-
-        if (C\contains_key($config, 'default_ttl')) {
-            /* HH_IGNORE_ERROR[4110] */
-            $defaultTtl = $config['default_ttl'] as int;
-        } else {
-            $defaultTtl = 0;
-        }
-
+        $store = $config['store'] ?? ($env->debug() ? 'null' : ($env->dev() ? 'array' : 'apc'));
+        $namespace = $config['namespace'] ?? '';
+        $defaultTtl = $config['default_ttl'] ?? 0;
+    
+        $this->share(Store\ApcStore::class)
+                ->addArgument(new RawArgument($namespace))
+                ->addArgument(new RawArgument($defaultTtl))
+                ->addArgument(SerializerInterface::class);
+        
+        $this->share(Store\ArrayStore::class)
+                ->addArgument(new RawArgument($defaultTtl));
+                
+        $this->share(Store\RedisStore::class)
+                ->addArgument(Redis::class)
+                ->addArgument(new RawArgument($namespace))
+                ->addArgument(new RawArgument($defaultTtl))
+                ->addArgument(SerializerInterface::class);
+        
+        $this->share(Store\NullStore::class);
+        
         switch ($store) {
             case 'apc':
-                $this->share(StoreInterface::class, Store\ApcStore::class)
-                    ->addArgument(new RawArgument($namespace))
-                    ->addArgument(new RawArgument($defaultTtl))
-                    ->addArgument(SerializerInterface::class);
+            case Store\ApcStore::class:
+                $store = Store\ApcStore::class;
                 break;
             case 'array':
-                $this->share(StoreInterface::class, Store\ArrayStore::class)
-                    ->addArgument(new RawArgument($defaultTtl));
+            case Store\ArrayStore::class:
+                $store = Store\ArrayStore::class;
                 break;
             case 'redis':
-                $this->share(StoreInterface::class, Store\RedisStore::class)
-                    ->addArgument(Redis::class)
-                    ->addArgument(new RawArgument($namespace))
-                    ->addArgument(new RawArgument($defaultTtl))
-                    ->addArgument(SerializerInterface::class);
+            case Store\RedisStore::class:
+                $store = Store\RedisStore::class;
                 break;
-            case 'null':
             default:
-                $this->share(StoreInterface::class, Store\NullStore::class);
+                $store = Store\NullStore::class;
                 break;
         }
+
+        $this->share(StoreInterface::class, () ==> {
+            return $this->getContainer()->get($store);
+        });
     }
 }
