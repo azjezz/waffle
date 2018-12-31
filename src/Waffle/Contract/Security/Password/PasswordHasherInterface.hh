cee78b160b9edd7df3997e5903a1b559448e2bcf
azjezz <azjezz@protonmail.com>
2018-12-31 18:09:47 +0100

    [Contract] add password contract

---
+++ src/Waffle/Contract/Security/Password/PasswordHasherInterface.hh
@@ -0,0 +1,32 @@
+<?hh // strict
+
+namespace Waffle\Contract\Security\Password;
+
+interface PasswordHasherInterface
+{
+    /**
+     * Create a password hash.
+     */
+    public function hash(string $password): string;
+
+    /**
+     * Check if the given hash matches the given password. 
+     */
+    public function check(string $password, string $hash): bool;
+
+    /**
+     * Check if the given hash matchs the password hasher
+     * algorithm.
+     */
+    public function match(string $hash): bool;
+
+    /**
+     * Returns information about the given hash.
+     *
+     * if the supplied hash doesn't belong to the current
+     * password hasher, this method MUST return null.
+     *
+     * @see match(strign $hash)
+     */
+    public function info(string $hash): ?shape(...);
+}
